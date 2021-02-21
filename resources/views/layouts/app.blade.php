58b6162239f955dd5832499a8819f9305af4793a
Nerijus Bartoševičius <nerijus.bartosevicius@gmail.com>
2021-02-21 20:15:09 +0200

    Added favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="icon" href="{{ asset('/images/favicon.ico') }}" type="image/x-icon"/>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
