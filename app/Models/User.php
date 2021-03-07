6a6f9b7ace49df0afe1e49ef8005ef26aeffaaa2
Nerijus Bartoševičius <nerijus.bartosevicius@gmail.com>
2021-03-07 21:31:55 +0200

    Added laravel/cashier library

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,10 +8,11 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Cashier\Billable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable, Timestamp;
+    use HasFactory, Notifiable, Timestamp,Billable;
 
     /**
      * The attributes that are mass assignable.
